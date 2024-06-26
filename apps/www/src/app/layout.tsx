2324b32ec676a10ff808cbc589b60d490ee4252b
Bruno Menezes <brunodmenezes@gmail.com>
2024-06-26 17:23:57 +1200

    feat: Add plausible

--- apps/www/src/app/layout.tsx
+++ apps/www/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import { ColorSchemeScript } from "@mantine/core";
 import "@mantine/core/styles.css";
 import type { Metadata } from "next";
+import { Plausible } from "../components/Plausible";
 import StyleProvider from "../providers/styleProvider";
 import { horizon } from "../styles/fonts";
 import "./globals.css";
@@ -18,6 +19,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                <Plausible />
                 <ColorSchemeScript />
             </head>
 
---
+++ apps/www/src/components/Plausible.tsx
@@ -0,0 +1,9 @@
+"use client";
+import { FC } from "react";
+
+const dataDomain = process.env.NEXT_PUBLIC_WWW_DOMAIN;
+const src = process.env.NEXT_PUBLIC_PLAUSIBLE_DOMAIN;
+
+export const Plausible: FC = () => {
+    return <script defer data-domain={dataDomain} src={src}></script>;
+};
