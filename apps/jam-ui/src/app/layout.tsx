c70698da7ed54d86a1f700b73967c9d702c67ce2
Bruno Menezes <brunodmenezes@gmail.com>
2024-06-28 18:18:32 +1200

    fix: Add correct favicon

--- apps/jam-ui/src/app/layout.tsx
+++ apps/jam-ui/src/app/layout.tsx
@@ -27,6 +27,7 @@ export default function RootLayout({
     return (
         <html lang="en">
             <head>
+                <link rel="icon" href="/favicon.ico" sizes="any" />
                 <Plausible />
                 <ColorSchemeScript />
             </head>
